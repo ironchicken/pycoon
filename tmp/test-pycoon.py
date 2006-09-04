9d42c7d998323bbd37f9e66dce5084849c7fefd7
Richard Lewis <richard@rjlewis.me.uk>
2006-09-04 08:52:27 +0000

    added test scripts to tmp

---
+++ tmp/test-pycoon.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import sys
+import pycoon
+
+SITES = {"home": {'SERVER_NAME': "localhost.localdomain",\
+                  'DOCUMENT_ROOT': "/home/richard/Documents/python",\
+                  'sitemap': "test-sitemap.xml"},\
+        "studio": {'SERVER_NAME': "localhost.localdomain",\
+                   'DOCUMENT_ROOT': "/var/www-studio",\
+                   'sitemap': "sitemap.xml"},\
+        "cursus": {'SERVER_NAME': "localhost.localdomain",\
+                   'DOCUMENT_ROOT': "/var/www-cursus",\
+                   'sitemap': "sitemap.xml"}}
+
+class server:
+    def __init__(self):
+        self.cleanup_func = None
+        self.server_hostname = ""
+
+    def register_cleanup(self, req, func):
+        self.cleanup_func = func
+
+class request:
+    def __init__(self, uri):
+        self.uri = uri
+        self.unparsed_uri = uri
+        self._content_type = ""
+        self.status = None
+        self.subprocess_env = SITES[sys.argv[1]]
+        self.server = server()
+        self.server.server_hostname = self.subprocess_env['SERVER_NAME']
+
+    def document_root(self):
+        return self.subprocess_env['DOCUMENT_ROOT']
+    
+    def set_content_type(self, c):
+        sys.stderr.write("Content-type: %s\n" % s)
+        self._content_type = c
+
+    def get_content_type(self): return self._content_type
+
+    content_type = property(fget=get_content_type, fset=set_content_type)
+    
+    def write(self, *args):
+        for a in args:
+            print a
+
+    def sendfile(self, fn):
+        f = open(fn, "r")
+        print f.read()
+        f.close()
+
+    def set_content_length(self, l):
+        sys.stderr.write("Content-length: %d\n" % l)
+
+    def add_common_vars(self):
+        pass
+
+r = request(sys.argv[2])
+
+res = pycoon.handler(r)
+print "Return code: %s" % res
+print "Return status: %s" % r.status
+
+r.server.cleanup_func(r)
