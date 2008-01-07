30e4d76aaa8e6ef46b53b59e40c9979e64785d14
anrienord <anrienord@78939a47-381c-0410-b85d-49bf6e4ba744>
2008-01-07 05:28:36 +0000

    Replaced flatten(xs) by equivalent and built-in sum(xs, [])
    * flatten() -> sum()
    * Minor cleanup

--- test/linecnt.py
+++ test/linecnt.py
@@ -1,36 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Shows line count in all files (specified by file mask) in the direcory. Useful
-for counting source code lines in a software project.
+"""\
+linecnt.py -- shows line count in all files (specified by file mask) in the direcory.
+It is useful for counting source code lines in a software project.
 
 Usage: linecnt.py <base directory> <file mask>
 """
 
 __author__ = "Andrey Nordin <http://claimid.com/anrienord>"
 
-import sys
-import os
+import sys, os
 from glob import glob
 
 def subdirs(d):
     return [os.path.join(d, f) for f in os.listdir(d) if os.path.isdir(os.path.join(d, f))]
     
 def alldirs(d):
-    return [d] + flatten([alldirs(f) for f in subdirs(d)])
+    return [d] + sum([alldirs(f) for f in subdirs(d)], [])
 
 def allfiles(d, m):
-    return flatten([glob(os.path.join(d, m)) for d in alldirs(directory)])
+    return sum([glob(os.path.join(d, m)) for d in alldirs(directory)], [])
 
 def strcnt(f):
     return len(open(f).read().splitlines())
 
-def flatten(list):
-    return reduce(lambda x, y: x + y, list, [])
-    
 if __name__ == "__main__":    
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print __doc__
         sys.exit(1)
     directory, mask = sys.argv[1:3]    
